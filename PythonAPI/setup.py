7e0487eb4b56bb91af30de19f53c2dbea613f8a3
Luis <jltobiasq@gmail.com>
2021-01-15 14:30:59 +0100

    Add install reqs

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -20,6 +20,10 @@ ext_modules = [
 setup(name='pycocotools',
       packages=['pycocotools'],
       package_dir = {'pycocotools': 'pycocotools'},
+      install_requires=[
+        'setuptools>=18.0',
+        'cython>=0.27.3',
+      ],
       version='2.0',
       ext_modules=
           cythonize(ext_modules)
